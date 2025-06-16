51741aeba9dabbd162b69e5c4df1aac280fbf0f8
André Luiz Lopes Furtado <144447401+Andre-Luiz-lopes@users.noreply.github.com>
2025-06-15 22:01:38 -0300

    Add files via upload

---
+++ electron-calendar-app/src/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // NÃO ESCONDA A BARRA DE MENU!
+  // mainWindow.setMenuBarVisibility(false);
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
